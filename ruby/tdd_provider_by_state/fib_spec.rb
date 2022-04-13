073e4ae51c072dd6fa522f47619c5d8abc2bfcb3
Greg Answer <greganswer@gmail.com>
2022-04-13 18:51:20 -0400

    Simplify fib specs

--- ruby/tdd_provider_by_state/fib_spec.rb
+++ ruby/tdd_provider_by_state/fib_spec.rb
@@ -3,10 +3,7 @@ require 'rspec'
 # [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
 
 RSpec.describe 'fib' do
-  subject { fib(n) }
-
   context 'when 0' do
-    let(:n) { 0 }
-    it { is_expected.to eq(0) }
+    it { expect(fib(0)).to eq(0) }
   end
 end
